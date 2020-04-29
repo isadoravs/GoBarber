f0b58ae1e8d9a3a8a76badc29d4d4f09d8513a58
Isadora Vasconcellos <vs.isadora@gmail.com>
2020-04-29 11:17:30 -0300

    Initial commit

--- gobarber-web/src/hooks/auth.tsx
+++ gobarber-web/src/hooks/auth.tsx
@@ -39,8 +39,8 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   const signOut = useCallback(() => {
-    localStorage.remove('@GoBarber:token');
-    localStorage.remove('@GoBarber:user');
+    localStorage.removeItem('@GoBarber:token');
+    localStorage.removeItem('@GoBarber:user');
 
     setData({} as AuthState);
   }, []);
